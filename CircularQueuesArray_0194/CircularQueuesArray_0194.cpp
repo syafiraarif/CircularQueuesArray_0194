32ac0fd01662fd6b3c392e76d43ad969177595c2
syairaarif <putrisyafira4444@gmail.com>
2024-05-30 11:06:13 +0700

    membuat if dalam methode display

--- CircularQueuesArray_0194/CircularQueuesArray_0194.cpp
+++ CircularQueuesArray_0194/CircularQueuesArray_0194.cpp
@@ -74,7 +74,16 @@ public:
 
     void display()
     {
+        //cek apakah antrian kosong
+        if(FRONT == -1)
+        {
+            cout << "Queue is empety\n";
+            return;
+        }
 
+        cout << "\nElement in the queue are....\n";
+
+        
     }
 };
 int main()
