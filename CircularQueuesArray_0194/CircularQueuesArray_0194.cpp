607e9a59a6d60cdad33b0dc2db81e2f46f940ba4
syairaarif <putrisyafira4444@gmail.com>
2024-05-30 11:01:54 +0700

    membuat program untuk tau beberapa kondisi dengan if else

--- CircularQueuesArray_0194/CircularQueuesArray_0194.cpp
+++ CircularQueuesArray_0194/CircularQueuesArray_0194.cpp
@@ -55,6 +55,21 @@ public:
             return;
         }
         cout << "\nThe element deleted from the queue is : " << queue_array[FRONT] << endl;
+
+        //cek jika antian hanya memiliki satu element
+        if(FRONT == REAR)
+        {
+            FRONT = -1;
+            REAR = -1;
+        }
+        else
+        {
+            //jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
+            if (FRONT == max - 1)
+                FRONT = 0;
+            else
+                FRONT = FRONT + 1;
+        }
     }
 };
 int main()
