86ae0c2687494b8376a2b194d888146df63c4d8e
syairaarif <putrisyafira4444@gmail.com>
2024-05-30 11:33:04 +0700

    membuat prorgam switch dalam program utama

--- CircularQueuesArray_0194/CircularQueuesArray_0194.cpp
+++ CircularQueuesArray_0194/CircularQueuesArray_0194.cpp
@@ -119,6 +119,50 @@ int main()
 {
     queues q;
     char ch;
+
+    while(true)
+    {
+        try
+        {
+            cout << "Menu" << endl;
+            cout << "1. Implement insert operation" << endl;
+            cout << "2. Implement delete operation" << endl;
+            cout << "3. Display value" << endl;
+            cout << "4. Exit" << endl;
+            cout << "Enter your choice(1-4):" << endl;
+            cin >> ch;
+            cout << endl;
+
+            switch (ch)
+            {
+            case '1':
+            {
+                q.insert();
+                break;
+            }
+            case '2':
+            {
+                q.remove();
+                break;
+            }
+            case '3':
+            {
+                q.display();
+                break;
+            }
+            case '4':
+            {
+                return 0;
+            }
+            default:
+            {
+                cout << "Invalid option!!" << endl;
+                break;
+            }
+            }
+             
+        }
+    }
     
 }
 
